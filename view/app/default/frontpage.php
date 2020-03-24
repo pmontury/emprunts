95f6a21919611fb1dbe1b615f02853fb7ac0525b
unknown <pmontury@yahoo.fr>
2020-03-24 16:03:53 +0100

    final 2

--- view/app/default/frontpage.php
+++ view/app/default/frontpage.php
@@ -2,8 +2,22 @@
     <?= $message; ?>
 </h1>
 <h1>Statistiques</h1>
-<p>Nombre d'abonnés: <?= $stats['nb_abonnes']; ?></p>
-<p>Nombre de produits: <?= $stats['nb_products']; ?></p>
-<p>Nombre de catégories: <?= $stats['nb_cats']; ?></p>
-<p>Nombre total d'emprunts: <?= $stats['nb_emprunts_tot']; ?></p>
-<p>Nombre d'emprunts en cours: <?= $stats['nb_emprunts_encours']; ?></p>
+<table class="stats">
+   <tbody>
+      <tr>
+         <td class="statstitle">Nombre d'abonnés</td><td>:</td><td><?= $stats['nb_abonnes']; ?></td>
+      </tr>
+      <tr>
+         <td class="statstitle">Nombre de produits</td><td>:</td><td><?= $stats['nb_products']; ?></td>
+      </tr>
+      <tr>
+         <td class="statstitle">Nombre de catégories</td><td>:</td><td><?= $stats['nb_cats']; ?></td>
+      </tr>
+      <tr>
+         <td class="statstitle">Nombre total d'emprunts</td><td>:</td><td><?= $stats['nb_emprunts_tot']; ?></td>
+      </tr>
+      <tr>
+         <td class="statstitle">Nombre d'emprunts en cours</td><td>:</td><td><?= $stats['nb_emprunts_encours']; ?></td>
+      </tr>
+   </tbody>
+</table>
